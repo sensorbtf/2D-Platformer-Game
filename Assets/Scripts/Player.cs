e7c6acd09052e3564072e2fd8da75dd7c6e775a8
Mateusz Łysoń <mateusz223311@gmail.com>
2022-07-21 13:04:36 +0200

    Revert "?"
    This reverts commit 282f1f23ee5ca2aa0698dc62d220550ba789a663.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,14 +27,12 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Transform _WallTouchingValidator;
 
-    private bool _jumpOffCoroutine = false;
+
 
     [SerializeField]
     private float _radiousChecker;
     [SerializeField]
     private LayerMask _whatIsPlatform;
-    [SerializeField]
-    private LayerMask _whatIsPlayer;
 
     // Wall-jumping fields
     private bool _isJumpingFromWall;
@@ -44,8 +42,6 @@ public class Player : MonoBehaviour
     private float _yWallForce;
     [SerializeField]
     private float _wallJumpTime;
-
-    // Properties for most used fields
     public float Speed 
     {
         get { return _speed; }
@@ -64,14 +60,10 @@ public class Player : MonoBehaviour
     private void Start()
     {
         RB2D = GetComponent<Rigidbody2D>();
-
-        _whatIsPlatform = LayerMask.NameToLayer("Platforms");
-        _whatIsPlayer = LayerMask.NameToLayer("Player");
     }
 
     private void Update()
     {
-        Debug.Log(_isGrounded);
         float input = Input.GetAxisRaw("Horizontal");
         RB2D.velocity = new Vector2(input * _speed, RB2D.velocity.y);
 
@@ -80,15 +72,8 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow) && _isGrounded == true)
         {
             RB2D.velocity = Vector2.up * JumpForce;
-            Debug.Log("Jumping");
         }
-        // Allowing player to jump down
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            JumpOff();
-            Debug.Log("JumpingOff");
-        }
-        // Flipping Hero Character
+        
         if (input > 0 && _isFacingRight == false)
         {
             FlipHero();
@@ -97,8 +82,6 @@ public class Player : MonoBehaviour
         {
             FlipHero();
         }
-
-        // Wall Jumping 
         if (_isTouchingWall == true && _isGrounded == false && input != 0)
         {
             _isSlidingWall = true;
@@ -107,41 +90,28 @@ public class Player : MonoBehaviour
         {
             _isSlidingWall = false;
         }
-
         if (_isSlidingWall)
         { 
             RB2D.velocity = new Vector2(RB2D.velocity.x, Mathf.Clamp(RB2D.velocity.y, -WallSlidingSpeed, float.MaxValue));
         }
-
         if (Input.GetKeyDown(KeyCode.UpArrow) && _isSlidingWall == true)
         { 
             _isJumpingFromWall = true;
             Invoke("SetWallJumpingToFalse", _wallJumpTime);
         }
-
         if (_isJumpingFromWall == true)
         {
             RB2D.velocity = new Vector2(_xWallForce * -input, _yWallForce);
         }
     }
-    // Method for flipping Hero left/right
+
     void FlipHero()
     {
        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
        _isFacingRight =! _isFacingRight;
     }
-    // Method for WallJumpingFeature
     void SetWallJumpingToFalse()
     {
         _isJumpingFromWall = false;
-    }
-    //Method for Moving From Platform
-    IEnumerator JumpOff()
-    {
-        _jumpOffCoroutine = true;
-        Physics2D.IgnoreLayerCollision(6, 10, true);
-        yield return new WaitForSeconds(0.5f);
-        Physics2D.IgnoreLayerCollision(6, 10, false);
-        _jumpOffCoroutine = false;
     }
 }
